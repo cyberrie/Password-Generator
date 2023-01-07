227ded1893e8be83061f10ef07b70bd7d75adc67
helena <helena.gilja@gmail.com>
2023-01-07 13:22:16 +0000

    left conditionals for password char inside the main function and also added an empty string in the function for password input to reset upon a new generation

--- final/script.js
+++ final/script.js
@@ -88,21 +88,21 @@ let upperCasedCharacters = [
   "Z",
 ];
 
-// Function to prompt user for password options & conditional while loop included when the generator worked with all conditions 'true'
+// Function to prompt user for password options
 function getPasswordOptions() {
   // Object storing all the options inside the function
   let passOptions = {};
 
+  // While loop used to indicate conditions for password length (stored into a variable)
   while (true) {
     passOptions.length = prompt("Enter Password Length (10-64):");
     if (passOptions.length >= 10 && passOptions.length <= 64) {
       break;
     } else {
-      alert("Error: Passwowrd must be between 10 and 64 characters.");
+      alert("Error: Password must be between 10 and 64 characters.");
     }
   }
   // Store options into variables
-
   passOptions.lower = confirm("Do you want to include Lowercase Characters?");
   passOptions.upper = confirm("Do you want to include Uppercase Characters?");
   passOptions.numeric = confirm("Do you want to include Numbers?");
@@ -117,27 +117,28 @@ function getRandom(arr) {
 }
 
 // testing getRandom with different arrays
-console.log(getRandom(numericCharacters));
+// console.log(getRandom(numericCharacters));
 
 // New variable for generated password
 let generatedPassword = "";
 // Function to generate password with user input
-// generate password runs when we click on generate button
+// This function runs when the user clicks on generate button
 function generatePassword() {
+  // set the generatedPassword to an empty string so it resets each time the button Generate Password is clicked
+  // generatedPassword = "";
   // new array to store the chose password characters
   let bigArray = [];
   // Storing password options function into a new variable
   const passwordOptions = getPasswordOptions();
 
-  // Check if at least one character type is selected
   if (
     !passwordOptions.lower &&
     !passwordOptions.upper &&
     !passwordOptions.numeric &&
     !passwordOptions.special
   ) {
-    alert("Error: You must select at least one character type.");
-    return; // after this the password returns undefined - am I able to reset the circle on pression ok?
+    alert("Error: You must select AT LEAST ONE character type.");
+    return; // this returns undefined when runs until the end, but if I return getPasswordOptions I then get an object as a result when all parameters followed.
   }
 
   if (passwordOptions.lower) {
