417f0afc72d3b8ef806062f69f28af281900cc42
helena <helena.gilja@gmail.com>
2023-01-10 22:39:32 +0000

    function created for character options and wrapped into while loop so it executes until one condition true, as opposed to returning a string. While loop for password length refactored

--- final/script.js
+++ final/script.js
@@ -88,25 +88,38 @@ let upperCasedCharacters = [
   "Z",
 ];
 
+// function to check for passwod characters, that at least one is selected
+function getCharOptions(passOptions) {
+  return (
+    passOptions.lower ||
+    passOptions.upper ||
+    passOptions.numeric ||
+    passOptions.special
+  );
+}
 // Function to prompt user for password options
 function getPasswordOptions() {
   // Object storing all the options inside the function
   let passOptions = {};
 
-  // While loop used to indicate conditions for password length (stored into a variable)
-  while (true) {
+  // Check for password length
+  passOptions.length = 0;
+  while (!(passOptions.length >= 10 && passOptions.length <= 64)) {
     passOptions.length = prompt("Enter Password Length (10-64):");
-    if (passOptions.length >= 10 && passOptions.length <= 64) {
-      break;
-    } else {
-      alert("Error: Password must be between 10 and 64 characters.");
-    }
   }
+
   // Store options into variables
-  passOptions.lower = confirm("Do you want to include Lowercase Characters?");
-  passOptions.upper = confirm("Do you want to include Uppercase Characters?");
-  passOptions.numeric = confirm("Do you want to include Numbers?");
-  passOptions.special = confirm("Do you want to include Special Characters?");
+  while (true) {
+    passOptions.lower = confirm("Do you want to include Lowercase Characters?");
+    passOptions.upper = confirm("Do you want to include Uppercase Characters?");
+    passOptions.numeric = confirm("Do you want to include Numbers?");
+    passOptions.special = confirm("Do you want to include Special Characters?");
+
+    // check if at least one character type has been selected
+    if (getCharOptions(passOptions)) {
+      break;
+    } else alert("Error: You must select AT LEAST ONE character type.");
+  }
   return passOptions;
 }
 
@@ -128,18 +141,6 @@ function generatePassword() {
   let bigArray = [];
   // Storing password options function into a new variable
   const passwordOptions = getPasswordOptions();
-
-  // this is to check character parameters
-  if (
-    !passwordOptions.lower &&
-    !passwordOptions.upper &&
-    !passwordOptions.numeric &&
-    !passwordOptions.special
-  ) {
-    return "Error: You must select AT LEAST ONE character type."; // this returns a string and the user has to Generate Password again to restart the process. Hope this is ok functionality wise, no indication was given wheter this should re-execute the function utomatically
-  }
-  /* P.S. I have attempted to return generatePassword function in the above conditionals, however this then returns double string, one of which is the actual password correctly generated (from 2nd execution, when at least one Boolean = true) and the concatenation is undefined*password length chosen by the user in the first execution when Booleans were all false.. I guess this is as it goes over to an empty array and stores undefined values 
-  I have also attempted while loop with Object.values(characterOptions).includes(true) in getPasswordOptions function and it also worked, but again string returned*/
 
   if (passwordOptions.lower) {
     // Create an array containing all selected options and concatenate further selected accordingly
@@ -163,6 +164,7 @@ function generatePassword() {
     generatedPassword += getRandom(bigArray);
   }
   console.log(generatedPassword, "Password Length", generatedPassword.length);
+
   return generatedPassword;
 }
 
